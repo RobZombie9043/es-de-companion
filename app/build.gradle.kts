8f61e8d3fd84fec3648d6451a614f16aeafd162e
RobZombie9043 <77545967+RobZombie9043@users.noreply.github.com>
2026-08-17 18:57:30 +1200

    Bump version to 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.esde.companion"
         minSdk = 29
         targetSdk = 35
-        versionCode = 28
-        versionName = "1.0.2"
+        versionCode = 29
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
